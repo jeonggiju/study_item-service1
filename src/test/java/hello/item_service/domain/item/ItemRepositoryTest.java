de2b21fa6bb5e647aa1e0f3a382d03e9a2804721
jeonggiju <halogiju123@gmail.com>
2025-02-14 00:04:53 +0900

    2025/02/14

---
+++ src/test/java/hello/item_service/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,69 @@
+package hello.item_service.domain.item;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach(){
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save(){
+        //given
+        Item itemA = new Item("itemA", 10000, 10);
+
+        //when
+        Item savedItem = itemRepository.save(itemA);
+
+        //then
+        Item findItem = itemRepository.findById(itemA.getId());
+        assertThat(findItem).isEqualTo(savedItem);
+    }
+
+    @Test
+    void findAll(){
+        //given
+        Item item1 = new Item("item1", 10000, 10);
+        Item item2 = new Item("item2", 20000, 20);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        //when
+        List<Item> all = itemRepository.findAll();
+
+        //then
+        assertThat(all.size()).isEqualTo(2);
+        assertThat(all).contains(item1, item2);
+    }
+
+    @Test
+    void updateItem(){
+        //given
+        Item itemA = new Item("itemA", 10000, 10);
+        Item savedItem = itemRepository.save(itemA);
+        Long itemId = itemA.getId();
+
+        // when
+        Item updateParam = new Item("updateParam", 20000, 20);
+        itemRepository.update(itemId, updateParam);
+
+        Item findItem = itemRepository.findById(itemId);
+
+        //then
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+
+    }
+}
